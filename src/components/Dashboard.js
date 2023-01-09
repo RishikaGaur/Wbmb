9526571f385c6fe70960a0b6b231ad13561eb27f
RishikaGaur <gaurrishika156@gmail.com>
2023-01-09 12:21:18 +0530

    my first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Dashboard.css"
+
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='first child'>
+          <span className='heading'>
+            List
+          </span>
+          <button className='new'>Add</button>
+        </div>
+        <div className='second child'>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Dashboard
